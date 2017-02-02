050173d2b3ee07548ac9326bdff9e397c3823bd5
YorchCb <yorchcb@gmail.com>
2017-02-02 09:23:14 -0300

    Agregados

---
+++ application/views/Plantillas/qs.php
@@ -0,0 +1,31 @@
+					<!-- Inicio QS -->
+						<a href="#qs" class="btn" data-toggle="modal"> ¿Quiénes somos? </a>
+						<div class="modal fade" id='qs'> 
+							<div class="modal-dialog">
+								<div class="modal-content">
+									<!-- Header de la ventana -->
+									<div class="modal-header">
+										<button tyle="button" class="close" data-dismiss="modal" aria-hidden='true'>&times;</button>
+										<h1 class="modal-title">
+											¿Quiénes somos?
+										</h1>
+									</div>
+									<!-- Contenido -->
+									<div class="modal-boddy">
+										<p>
+											Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+											tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+											quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+											consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+											cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+											proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+										</p>
+									</div>
+									<!-- Footer -->
+									<div class="modal-footer">
+										<button type="button" class="btn btn-primary" data-dismiss='modal'>Cerrar</button>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- Fin QS -->
