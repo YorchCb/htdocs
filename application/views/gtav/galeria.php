ed021d5bcf190c2f75d025110de2fe590a65692f
YorchCb <yorchcb@gmail.com>
2017-02-04 04:33:55 -0300

    Agregados

--- application/views/gtav/galeria.php
+++ application/views/gtav/galeria.php
@@ -1,1 +1,10 @@
-Hola
+<a href="/gtav">volver</a>
+<div id="albumes" class="col-xs-12">
+	hola
+</div>
+<div id="visualizacion" class="col-xs-8">
+	<img src="<?php echo base_url(); ?>images/galeria.png">
+</div>
+<div id="imagenes" class="col-xs-4">
+	hola
+</div>
